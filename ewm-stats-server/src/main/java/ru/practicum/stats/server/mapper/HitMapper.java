6ade6fffd86b1c4e44e5144104eaa74f193194bc
Aleks Zaripov <lexazarip2005@gmail.com>
2025-12-25 23:12:43 +0300

    Исправление по комментариям

--- ewm-stats-server/src/main/java/ru/practicum/stats/server/mapper/HitMapper.java
+++ ewm-stats-server/src/main/java/ru/practicum/stats/server/mapper/HitMapper.java
@@ -1,10 +1,12 @@
 package ru.practicum.stats.server.mapper;
 
+import org.springframework.stereotype.Component;
 import ru.practicum.dto.EndpointHitDto;
 import ru.practicum.stats.server.entity.Hit;
 
+@Component
 public class HitMapper {
-    public static Hit toEntity(EndpointHitDto dto) {
+    public Hit toEntity(EndpointHitDto dto) {
         Hit hit = new Hit();
         hit.setApp(dto.getApp());
         hit.setUri(dto.getUri());
@@ -13,7 +15,7 @@ public class HitMapper {
         return hit;
     }
 
-    public static EndpointHitDto toDto(Hit hit) {
+    public EndpointHitDto toDto(Hit hit) {
         return EndpointHitDto.builder()
                 .id(hit.getId())
                 .app(hit.getApp())
