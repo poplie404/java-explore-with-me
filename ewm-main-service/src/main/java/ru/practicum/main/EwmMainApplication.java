7a4d33a012a8427d7398f3387006462ac95cef12
Aleks Zaripov <lexazarip2005@gmail.com>
2025-12-27 15:22:54 +0300

    Реализация Этапа 2 Дипломного проекта

--- ewm-main-service/src/main/java/ru/practicum/main/EwmMainApplication.java
+++ ewm-main-service/src/main/java/ru/practicum/main/EwmMainApplication.java
@@ -3,7 +3,7 @@ package ru.practicum.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"ru.practicum.main", "ru.practicum.stats.client"})
 public class EwmMainApplication {
     public static void main(String[] args) {
         SpringApplication.run(EwmMainApplication.class, args);
